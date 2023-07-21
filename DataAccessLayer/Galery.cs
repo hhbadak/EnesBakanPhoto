7a03d0c499396d3ea00922763ac4af01b5e9ce39
Hasan Hüseyin BADAK <hsnhsynesk@gmail.com>
2023-07-21 23:45:49 +0300

    is continued

---
+++ DataAccessLayer/Galery.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public class Galery
+    {
+        public int ID { get; set; }
+        public string media { get; set; }
+        public string category { get; set; }
+        public int category_ID { get; set; }
+    }
+}
