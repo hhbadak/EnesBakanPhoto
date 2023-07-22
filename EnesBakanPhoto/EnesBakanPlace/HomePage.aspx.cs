cced59e29e149ff61ad39e06949b1995f5692324
Hacı Mustafa <hacims85@hotmail.com>
2023-07-22 09:44:43 +0300

    .

--- EnesBakanPhoto/EnesBakanPlace/HomePage.aspx.cs
+++ EnesBakanPhoto/EnesBakanPlace/HomePage.aspx.cs
@@ -7,7 +7,7 @@ using System.Web.UI.WebControls;
 
 namespace EnesBakanPhoto.EnesBakanPlace
 {
-    public partial class HomePage : System.Web.UI.MasterPage
+    public partial class HomePage : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
